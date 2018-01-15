00d43b3a909ffc37981557735a9a727f7f28ffd6
Léo NOEL <leo.noel.fr@gmail.com>
2018-01-15 11:38:49 +0100

    CHORE: bump dep versions [clojure plop]

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "https://github.com/leonoel/task"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/clojurescript "1.9.946"]
-                 [plop "a.1"]])
+                 [plop "a.2"]])
